eed08b25b354bc013ccbd0e79988ed6264ea0985
mickulis <mkulis@sigma.ug.edu.pl>
2019-11-02 11:06:37 +0100

    .

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 gem 'bundler'
 gem 'minitest'
+gem 'minitest-reporters', '>= 0.5.0'
 gem 'rspec'
 gem 'simplecov'
--- test/hello_world_test.rb
+++ test/hello_world_test.rb
@@ -1,15 +1,6 @@
-begin
-  gem 'minitest', '>= 5.0.0'
-  require 'minitest/autorun'
-  require_relative '../lib/hello_world'
-rescue Gem::LoadError => e
-  puts "\nMissing Dependency:\n#{e.backtrace.first} #{e.message}"
-  puts 'Minitest 5.0 gem must be installed for the Ruby track.'
-rescue LoadError => e
-  puts "\nError:\n#{e.backtrace.first} #{e.message}"
-  puts DATA.read
-  exit 1
-end
+require 'minitest/autorun'
+require_relative '../lib/hello_world'
+
 
 # Common test data version: 1.1.0 be3ae66
 class HelloWorldTest < Minitest::Test
