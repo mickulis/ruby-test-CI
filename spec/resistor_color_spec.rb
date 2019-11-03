63d2e6f94fc56e8df9a296488b2b9c212ee271c2
mickulis <mkulis@sigma.ug.edu.pl>
2019-11-03 15:33:21 +0100

    7 spec

--- spec/resistor_color_spec.rb
+++ spec/resistor_color_spec.rb
@@ -3,24 +3,19 @@ require_relative '../lib/resistor_color'
 # Common test data version: 1.0.0 edf1778
 RSpec.describe CLASS do
 	it "test_black" do
-		expect().to(eq())
-		0, ResistorColor.color_code("black")
+		expect(ResistorColor.color_code("black")).to(eq(expected))
 	end
 
 	it "test_white" do
-		expect().to(eq())
-		9, ResistorColor.color_code("white")
+		expect(ResistorColor.color_code("white")).to(eq(expected))
 	end
 
 	it "test_orange" do
-		expect().to(eq())
-		3, ResistorColor.color_code("orange")
+		expect(ResistorColor.color_code("orange")).to(eq(expected))
 	end
 
 	it "test_colors" do
-
 		expected = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
-		expect().to(eq())
-		expected, ResistorColor::COLORS
+		expect(ResistorColor::COLORS).to(eq(expected))
 	end
 end
--- spec/series_spec.rb
+++ spec/series_spec.rb
@@ -1,114 +1,85 @@
 require_relative '../lib/series'
 
-RSpec.describe CLASS do
+RSpec.describe Series do
 	it "test_simple_slices_of_one" do
 		series = Series.new('01234')
-		expect().to(eq())
-		['0', '1', '2', '3', '4'], series.slices(1)
+		expect(series.slices(1)).to(eq(['0', '1', '2', '3', '4']))
 	end
 
 	it "test_simple_slices_of_one_again" do
-
 		series = Series.new('92834')
-		expect().to(eq())
-		['9', '2', '8', '3', '4'], series.slices(1)
+		expect(series.slices(1)).to(eq(['9', '2', '8', '3', '4']))
 	end
 
 	it "test_simple_slices_of_two" do
-
 		series = Series.new('01234')
-		expect().to(eq())
-		['01', '12', '23', '34'], series.slices(2)
+		expect(series.slices(2)).to(eq(['01', '12', '23', '34']))
 	end
 
 	it "test_other_slices_of_two" do
-
 		series = Series.new('98273463')
 		expected = ['98', '82', '27', '73', '34', '46', '63']
-		expect().to(eq())
-		expected, series.slices(2)
+		expect(series.slices(2)).to(eq(expected))
 	end
 
 	it "test_simple_slices_of_two_again" do
-
 		series = Series.new('37103')
-		expect().to(eq())
-		['37', '71', '10', '03'], series.slices(2)
+		expect(series.slices(2)).to(eq(['37', '71', '10', '03']))
 	end
 
 	it "test_simple_slices_of_three" do
-
 		series = Series.new('01234')
-		expect().to(eq())
-		['012', '123', '234'], series.slices(3)
+		expect(series.slices(3)).to(eq(['012', '123', '234']))
 	end
 
 	it "test_simple_slices_of_three_again" do
-
 		series = Series.new('31001')
-		expect().to(eq())
-		['310', '100', '001'], series.slices(3)
+		expect(series.slices(3)).to(eq(['310', '100', '001']))
 	end
 
 	it "test_other_slices_of_three" do
-
 		series = Series.new('982347')
-		expect().to(eq())
-		['982', '823', '234', '347'], series.slices(3)
+		expect(series.slices(3)).to(eq(['982', '823', '234', '347']))
 	end
 
 	it "test_simple_slices_of_four" do
-
 		series = Series.new('01234')
-		expect().to(eq())
-		['0123', '1234'], series.slices(4)
+		expect(series.slices(4)).to(eq(['0123', '1234']))
 	end
 
 	it "test_simple_slices_of_four_again" do
-
 		series = Series.new('91274')
-		expect().to(eq())
-		['9127', '1274'], series.slices(4)
+		expect(series.slices(4)).to(eq(['9127', '1274']))
 	end
 
 	it "test_simple_slices_of_five" do
-
 		series = Series.new('01234')
-		expect().to(eq())
-		['01234'], series.slices(5)
+		expect(series.slices(5)).to eq(['01234'])
 	end
 
 	it "test_simple_slices_of_five_again" do
-
 		series = Series.new('81228')
-		expect().to(eq())
-		['81228'], series.slices(5)
+		expect(series.slices(5)).to eq(['81228'])
 	end
 
 	it "test_simple_slice_that_blows_up" do
-
 		series = Series.new('01234')
-		assert_raises ArgumentError do
+		expect do
 			series.slices(6)
-		end
+		end.to(raise_error(ArgumentError))
 	end
 
 	it "test_more_complicated_slice_that_blows_up" do
-
 		slice_string = '01032987583'
-
 		series = Series.new(slice_string)
-		assert_raises ArgumentError do
-			series.slices(slice_string.length + 1)
-		end
+		expect do
+			series.slices((slice_string.length + 1))
+		end.to(raise_error(ArgumentError))
 	end
 
 	it "test_sequential_slices" do
-
 		series = Series.new('1234')
-		expect().to(eq())
-		['12', '23', '34'], series.slices(2)
-		expect().to(eq())
-		['123', '234'], series.slices(3)
+		expect(series.slices(2)).to(eq(['12', '23', '34']))
+		expect(series.slices(3)).to(eq(['123', '234']))
 	end
 end
