76c6005f101c1b7347304af61f72089260f30777
mickulis <mkulis@sigma.ug.edu.pl>
2019-11-04 01:14:51 +0100

    connect fix

--- lib/connect.rb
+++ lib/connect.rb
@@ -1,82 +1,34 @@
-require 'set'
-
 class Board
-	def initialize(board)
-		@parsedBoard = []
-		for row in board
-			@parsedBoard.append(row.strip.split(" ").reject { |s| s.empty? })
-		end
-		@height = @parsedBoard.length
-		@width = @parsedBoard[0].length
+	def initialize(game)
+		@game = game.map { |row| row.split(' ') }
+		@o_set = find_indexes(@game, 'O')
+		@x_set = find_indexes(@game.transpose, 'X')
 	end
-
 
 	def winner
-		if x_wins?
-			"X"
-		elsif y_wins?
-			"O"
-		else
-			""
-		end
+		winner?(@o_set) ? 'O' : winner?(@x_set) ? 'X' : ''
 	end
 
-	def x_wins?
-		visited_x = Set[]
-		x_won = false
-		for i in 0..@height
-			x_won = x_won || move_x(0, i, visited_x)
-		end
-		x_won
+	private
+
+	def find_indexes(board, player)
+		board.map { |row| row.map.with_index { |c, i| i if c == player }.compact }
 	end
 
-	def move_x(x, y, visited)
-		if visited === "#{x} #{y}" or x < 0 or !y.between?(0, @height - 1)
-			false
-		elsif x == @width
-			true
-		else
-			visited << "#{x} #{y}"
-			if @parsedBoard[y][x] == "X"
-				move_x(x-1, y  , visited) or
-				move_x(x-1, y+1, visited) or
-				move_x(x  , y-1, visited) or
-				move_x(x  , y+1, visited) or
-				move_x(x+1, y-1, visited) or
-				move_x(x+1, y  , visited)
-			else
-				false
-			end
-		end
+	def winner?(board)
+		board.first.any? { |start| check_paths(board, 0, start) }
 	end
 
-
-	def y_wins?
-		visited_y = Set[]
-		y_won = false
-		for i in 0..@width
-			y_won = y_won || move_y(i, 0, visited_y)
+	def check_paths(board, row, col, path = [])
+		return true if row == board.length - 1
+		next_move = valid_moves(row, col).find do |next_row, next_col|
+			board[next_row].member?(next_col) && !path.member?([next_row, next_col])
 		end
-		y_won
+		next_move.nil? ? false : check_paths(board, *next_move, path << next_move)
 	end
 
-	def move_y(x, y, visited)
-		if visited === "#{x} #{y}" or !x.between?(0, @width - 1) or y < 0
-			false
-		elsif y == @height
-			true
-		else
-			visited.add("#{x} #{y}")
-			if @parsedBoard[y][x] == "O"
-				move_y(x-1, y  , visited) or
-				move_y(x-1, y+1, visited) or
-				move_y(x  , y-1, visited) or
-				move_y(x  , y+1, visited) or
-				move_y(x+1, y-1, visited) or
-				move_y(x+1, y  , visited)
-			else
-				false
-			end
-		end
+	def valid_moves(row, col)
+		[[row + 1, col], [row + 1, col - 1], [row, col + 1], [row, col - 1],
+		 [row - 1, col], [row - 1, col + 1]]
 	end
 end
