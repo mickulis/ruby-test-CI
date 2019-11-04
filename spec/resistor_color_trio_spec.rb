a0bc5ca035f84a0366ba1fb3d35d59b7400a3bbd
mickulis <mkulis@sigma.ug.edu.pl>
2019-11-04 12:47:14 +0100

    bugfix

--- spec/resistor_color_trio_spec.rb
+++ spec/resistor_color_trio_spec.rb
@@ -1,27 +1,27 @@
 require_relative '../lib/resistor_color_trio'
 
 RSpec.describe ResistorColorTrio do
-  def test_orange_and_orange_and_black
+  it "test_orange_and_orange_and_black" do
     expect(ResistorColorTrio.new(["orange", "orange", "black"]).label).to(eq("Resistor value: 33 ohms"))
   end
 
-  def test_blue_and_grey_and_brown
+  it "test_blue_and_grey_and_brown" do
     expect(ResistorColorTrio.new(["blue", "grey", "brown"]).label).to(eq("Resistor value: 680 ohms"))
   end
 
-  def test_red_and_black_and_red
+  it "test_red_and_black_and_red" do
     expect(ResistorColorTrio.new(["red", "black", "red"]).label).to(eq("Resistor value: 2 kiloohms"))
   end
 
-  def test_green_and_brown_and_orange
+  it "test_green_and_brown_and_orange" do
     expect(ResistorColorTrio.new(["green", "brown", "orange"]).label).to(eq("Resistor value: 51 kiloohms"))
   end
 
-  def test_yellow_and_violet_and_yellow
+  it "test_yellow_and_violet_and_yellow" do
     expect(ResistorColorTrio.new(["yellow", "violet", "yellow"]).label).to(eq("Resistor value: 470 kiloohms"))
   end
 
-  def test_invalid_color
+  it "test_invalid_color" do
     expect do
       ResistorColorTrio.new(["yellow", "purple", "black"]).label
     end.to(raise_error(ArgumentError))
